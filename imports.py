0b248910479d22fa65650eaf5e3638faa5562eeb
EricOdle <72859331+ericodle@users.noreply.github.com>
2024-01-03 10:20:04 +0900

    Delete imports.py

--- imports.py
+++
@@ -1,9 +0,0 @@
-# imports.py
-import numpy as np
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-from functions import (
-    hemisphere_surface, prolate_surface, oblate_surface, branching_surface,
-    hemisphere_planar, prolate_planar, oblate_planar, branching_planar,
-    hemisphere_svr, prolate_svr, oblate_svr, branching_svr
-)
