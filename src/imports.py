ebbdc03f5cd0a3462014102aca8939690dc8d0c6
EricOdle <72859331+ericodle@users.noreply.github.com>
2024-01-03 10:23:14 +0900

    Update imports.py

--- src/imports.py
+++ src/imports.py
@@ -2,8 +2,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-from functions import (
-    hemisphere_surface, prolate_surface, oblate_surface, branching_surface,
-    hemisphere_planar, prolate_planar, oblate_planar, branching_planar,
-    hemisphere_svr, prolate_svr, oblate_svr, branching_svr
-)
+from functions import *
+from plotting import *
