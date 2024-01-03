405fdc0313d3d4bbe253868826987527a75bc064
EricOdle <72859331+ericodle@users.noreply.github.com>
2024-01-03 10:19:15 +0900

    Add files via upload

---
+++ src/constants.py
@@ -0,0 +1,12 @@
+# constants
+G = 1.66
+rho = 1.4
+
+# Define the range of r and e values
+r = np.linspace(2, 50, 100)  # Range of semi-major axis
+e_oblate = np.linspace(-0.9, 0, 100)  # Range of eccentricity for oblate
+e_prolate = np.linspace(0, 0.9, 100)  # Range of eccentricity for prolate
+
+# Define the range of h and a values
+h = np.linspace(2, 50, 100)  # Range of semi-major axis
+a = np.linspace(0, 10, 100)  # Range of eccentricity for oblate
