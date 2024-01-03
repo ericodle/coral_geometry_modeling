615fcbaca1c7abc3bdb3b129cda59890e4555673
EricOdle <72859331+ericodle@users.noreply.github.com>
2024-01-03 10:27:40 +0900

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,25 @@
+# main.py
+import imports
+
+def main():
+    # Set your constants (G, rho, etc.)
+    G = 1.66
+    rho = 1.4
+
+    # Generate plots for hemispheroid surface area productivity
+    imports.hemispheroid_SAP()
+
+    # Generate plots for hemispheroid planar area productivity
+    imports.hemispheroid_PAP()
+
+    # Generate plots for hemispheroid surface area to volume ratio
+    imports.hemispheroid_SVR()
+
+    # Generate plots for branching coral surface area productivity
+    imports.branching_SAP()
+
+    # Generate plots for branching coral surface area to volume ratio
+    imports.branching_SVR()
+
+if __name__ == '__main__':
+    main()
